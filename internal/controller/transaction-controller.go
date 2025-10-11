7397d4ae1b8ab1d97f7ca2322a69973a17ba0170
jegasape <jegasape@gmail.com>
2025-10-11 12:08:42 -0400

    feat: added transaction-controller.go
    Signed-off-by: jegasape <jegasape@gmail.com>

---
+++ internal/controller/transaction-controller.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jegasape/spirex.git/internal/entity"
+	"github.com/jegasape/spirex.git/internal/service"
+)
+
+type TransactionController interface {
+	Add(w http.ResponseWriter, r *http.Request)
+	FindAll(w http.ResponseWriter, r *http.Request)
+}
+
+type controller struct {
+	service service.TransactionService
+}
+
+func New(service service.TransactionService) TransactionController {
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) Add(w http.ResponseWriter, r *http.Request) {
+	var entity entity.Detail
+	if err := json.NewDecoder(r.Body).Decode(&entity); err != nil {
+		http.Error(w, "Invalid payload...", http.StatusBadRequest)
+		return
+	}
+
+	c.service.Add(entity)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&entity)
+}
+
+func (c *controller) FindAll(w http.ResponseWriter, r *http.Request) {
+	entity := c.service.FindAll()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&entity)
+}
