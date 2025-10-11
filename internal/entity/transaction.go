d7260ac42697583f6a1a88094f585d9d7fa9532e
jegasape <jegasape@gmail.com>
2025-10-11 16:41:52 -0400

    fix: json targs change to lowercase

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -2,16 +2,16 @@ package entity
 
 type Detail struct {
 	Periodo               string  `json:"periodo"`
-	TipoEntidad           string  `json:"tipoEntidad"`
+	TipoEntidad           string  `json:"tipoentidad"`
 	Entidad               string  `json:"entidad"`
 	Region                string  `json:"region"`
 	Provincia             string  `json:"provincia"`
 	Persona               string  `json:"persona"`
 	Genero                string  `json:"genero"`
-	TipoCliente           string  `json:"tipoCliente"`
-	InstrumentoCaptacion  string  `json:"instrumentoCaptacion"`
+	TipoCliente           string  `json:"tipocliente"`
+	InstrumentoCaptacion  string  `json:"instrumentocaptacion"`
 	Divisa                string  `json:"divisa"`
-	CantidadInstrumento   int     `json:"cantidadInstrumento"`
+	CantidadInstrumento   int     `json:"cantidadinstrumento"`
 	Balance               float64 `json:"balance"`
-	TasaPromedioPonderado float64 `json:"tasaPromedioPonderado"`
+	TasaPromedioPonderado float64 `json:"tasapromedioponderado"`
 }
