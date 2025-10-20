9c2dd443b6cefa183934180094216ce54846f619
jegasape <jegasape@gmail.com>
2025-10-20 00:54:55 -0400

    feat: added edit and delete methods

--- internal/service/transaction-service.go
+++ internal/service/transaction-service.go
@@ -1,9 +1,13 @@
 package service
 
-import "github.com/jegasape/spirex.git/internal/entity"
+import (
+	"github.com/jegasape/spirex.git/internal/entity"
+)
 
 type TransactionService interface {
 	Add(entity.Detail) entity.Detail
+	Edit(entity.Detail) entity.Detail
+	Delete(entity.Detail) entity.Detail
 	FindAll() []entity.Detail
 }
 
@@ -18,6 +22,26 @@ func New() TransactionService {
 func (ts *transactionService) Add(v entity.Detail) entity.Detail {
 	ts.transations = append(ts.transations, v)
 	return v
+}
+
+func (ts *transactionService) Edit(v entity.Detail) entity.Detail {
+	for i, e := range ts.transations {
+		if e.Id == v.Id {
+			ts.transations[i] = v
+			return v
+		}
+	}
+	return entity.Detail{}
+}
+
+func (ts *transactionService) Delete(v entity.Detail) entity.Detail {
+	for i, e := range ts.transations {
+		if e.Id == v.Id {
+			ts.transations = append(ts.transations[:i], ts.transations[i+1:]...)
+			return v
+		}
+	}
+	return entity.Detail{}
 }
 
 func (ts *transactionService) FindAll() []entity.Detail {
